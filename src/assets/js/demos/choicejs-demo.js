ad51a0e32bd14c9c8fd74259637de24296a4cf38
SyntiaDewi <044502248@ecampus.ut.ac.id>
2025-06-25 17:50:03 +0700

    update repo menjadi openaccounting

--- src/assets/js/demos/choicejs-demo.js
+++
@@ -1,9 +0,0 @@
-import Choices from 'choices.js';
-
-var choiceElements = document.querySelectorAll('#choice-js');
-choiceElements.forEach((element) => {
-    new Choices(element, {
-        allowHTML: true,
-        shouldSort: false,
-    });
-});
